75dd58a0c748d7b6cd647fbabde86cb139ac665a
zakennayo-git <lukasz.gorka95@mgail.com>
2019-03-06 21:05:11 +0100

    Error heading

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ErrorInterceptorProvide } from './_services/error.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,7 +24,8 @@ import { RegisterComponent } from './register/register.component';
     FormsModule
   ],
   providers: [
-    AuthService
+    AuthService,
+    ErrorInterceptorProvide
   ],
   bootstrap: [AppComponent]
 })
